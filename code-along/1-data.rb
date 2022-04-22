bbaa38bed25b3a8e991b139d6bf106838e30440a
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-04-22 18:54:30 +0000

    1-data

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -2,18 +2,30 @@
 # is the same as where this file is located and then run:
 # ruby 1-data.rb
 
+# puts "Hello, world" 
+
+
 # In Ruby, there are different *types* of data:
 
 # Numbers
+# puts 7
 
 # Perform simple math with numbers
+# puts 5+2
 
 # Strings
+# puts "tacos"
 
 # Combine strings together
+# puts "#{3+3} tacos"
 
 # Variables
+#quantity = 3+3+3
+#food = "tacos"
 
 # Combine strings and variables
-
+#puts "#{quantity} #{ food}"
 # String manipulation
+# puts "Hello".upcase
+# puts "Hello".reverse
+# puts "Hello".swapcase
