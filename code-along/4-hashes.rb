03ebe584031188d411b349d6c42f411bbb64feeb
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-04-22 20:32:00 +0000

    4

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,19 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
-
+me = {
+    "name" => "Brian",
+    "location" => {"city" => "Chicago", "state" => "IL"},
+    "status" => "Teaching at Kellogg"
+}
+puts me
 # Accessing data from the hash
+puts me["name"]
+puts me["location"]["city"]
+
+me["name"] = {"first_name" => "brian", "last_name" => "eng"}
+puts me["name"]["first_name"]
 
 # More Complex Hashes
+
+
